1dbfb7843d0ca964d0aa1c89261a98f56884286c
Nymfeparakit <maxlex047@gmail.com>
2024-03-16 10:11:45 +0300

    Add cors middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.router import router as users_router
 from src.feeds.router import feeds_router
@@ -6,6 +7,16 @@ from src.folders.router import folders_router
 from src.sources.router import sources_router
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router, prefix="/auth", tags=["auth"])
 app.include_router(folders_router, prefix="/folders", tags=["folders"])
